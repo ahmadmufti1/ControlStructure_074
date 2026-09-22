4696bab1d6193782c265daa3f8162a816f89f569
ahmadmufti1 <muftiahmad617@gmail.com>
2026-09-22 11:04:28 +0700

    Mencari angka ganjil dari jumlah n

--- tugas 1/tugas2.py
+++ tugas 1/tugas2.py
@@ -2,9 +2,11 @@ a = int(input("Masukkan angka pertama: "))
 b = int(input("Masukkan angka kedua: "))
 c = int(input("Masukkan angka ketiga: "))
 
-if a >= b and a >= c:
+if a > b and a > c:
     print("Angka terbesar adalah:", a)
-elif b >= a and b >= c:
+elif b > a and b > c:
     print("Angka terbesar adalah:", b)
-elif c >= a and c >= b:
+elif c > a and c > b:
     print("Angka terbesar adalah:", c)
+else:
+    print("tidak ada nilai terbesar")
---
+++ tugas 1/tugas4.py
@@ -0,0 +1,3 @@
+n = int (input("Masukkan angka anda: "))
+for i in range (1, n+1 , 2):
+    print(i)
