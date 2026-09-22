8b0e1f9c7ee5e0727c669b9e6eb5c94ec4a5020b
ahmadmufti1 <muftiahmad617@gmail.com>
2026-09-22 10:24:52 +0700

    program mencari angka terbesar

---
+++ tugas 1/tugas2.py
@@ -0,0 +1,10 @@
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+c = int(input("Masukkan angka ketiga: "))
+
+if a >= b and a >= c:
+    print("Angka terbesar adalah:", a)
+elif b >= a and b >= c:
+    print("Angka terbesar adalah:", b)
+elif c >= a and c >= b:
+    print("Angka terbesar adalah:", c)
